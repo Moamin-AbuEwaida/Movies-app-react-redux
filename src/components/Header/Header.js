f5e2746065697a46f0560b97c9f80b0b5805c2f1
Moamin <moamin.salamah@gmail.com>
2022-10-04 10:44:14 +0200

    setting gh pages deployment

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,14 @@ import './Header.scss'
 
 
 const Header = () => {
+  
   return (
     <div className="header">
       <Link to='/'>
         <div className="logo">Movie App</div>
 
       </Link>
+      
       <div className="user-image">
         <img src={user} alt='user' />
       </div>
